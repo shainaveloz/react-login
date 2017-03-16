85a545f07cf1488c15bdfd9c3d14bb90805a603e
shainaveloz <shaina.veloz@gmail.com>
2017-03-16 19:37:58 -0400

    initial commit

---
+++ dist/webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+};
+//# sourceMappingURL=webpack.config.js.map
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+};
